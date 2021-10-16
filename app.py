717aab57956ba2580b40f502a64da40e7617fbc5
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2021-10-17 01:48:45 +0700

    feat: k medroid clustering end point

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def health_check():
     return 'OK'
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/predict/agglomerative-clustering', methods=['POST'])
+def predict_agglomerative_clustering():
     if request.method == 'POST' and request.json is not None:
         if is_production:
             token = request.headers.get('Authorization').split(' ')[1]
@@ -28,6 +28,24 @@ def predict():
         if data is not None:
             try:
                 result = model.predict(data)
+                return jsonify({"result": result})
+            except Exception as e:
+                print(data, flush=True)
+                print(e, flush=True)
+                return jsonify({"error": "Data format wrong"}), 400
+    return jsonify({"error": "Data is invalid or not exist"}), 400
+
+@app.route('/predict/k-medoids', methods=['POST'])
+def predict_k_medoids():
+    if request.method == 'POST' and request.json is not None:
+        if is_production:
+            token = request.headers.get('Authorization').split(' ')[1]
+            if token != authentication_token:
+                return jsonify({"error": "Unauthorized request"}), 403
+        data = request.json['data']
+        if data is not None:
+            try:
+                result = model.k_medoids_clustering(data)
                 return jsonify({"result": result})
             except Exception as e:
                 print(data, flush=True)
